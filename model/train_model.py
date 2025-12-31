69a475b7f1298b9fc4a7782b5292fa9ce9ed391d
jahnavijagasandram <jjahnavi2502@gmail.com>
2025-12-31 05:46:29 +0530

    testing file

--- model/train_model.py
+++ model/train_model.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-# Load dataset
+# Load dataset to project
 data = pd.read_csv("dataset.csv")
 
 # Features and label
